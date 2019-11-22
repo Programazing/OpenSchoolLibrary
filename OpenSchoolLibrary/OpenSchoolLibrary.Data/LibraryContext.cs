5f65a9fd546a304a8cd7258b9685f736ff8b543f
Programazing <chris@thatamazingprogrammer.com>
2019-11-21 21:39:25 -0500

    Add OpenSchoolLibrary.Data

---
+++ OpenSchoolLibrary/OpenSchoolLibrary.Data/LibraryContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OpenSchoolLibrary.Data
+{
+    class LibraryContext : DbContext
+    {
+    }
+}
