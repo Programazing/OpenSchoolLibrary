2e5bf512700edec2b3abbff18f430851ca802bbb
Programazing <chris@thatamazingprogrammer.com>
2019-11-24 01:09:15 -0500

    Add LibraryContext

--- OpenSchoolLibrary/OpenSchoolLibrary.Data/LibraryContext.cs
+++ OpenSchoolLibrary/OpenSchoolLibrary.Data/LibraryContext.cs
@@ -1,11 +1,24 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using OpenSchoolLibrary.Common.Models;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace OpenSchoolLibrary.Data
 {
-    class LibraryContext : DbContext
+    public class LibraryContext : DbContext
     {
+        public LibraryContext(DbContextOptions<LibraryContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Book> Books { get; set; }
+        public DbSet<BookLoan> BookLoans { get; set; }
+        public DbSet<Dewey> Deweys { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Teacher> Teachers { get; set; }
     }
 }
