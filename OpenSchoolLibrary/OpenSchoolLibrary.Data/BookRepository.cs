1f08d61bf3ff190fd4ac60488dd308eeb744d358
Programazing <chris@thatamazingprogrammer.com>
2019-11-24 01:09:38 -0500

    Add BookRepository

---
+++ OpenSchoolLibrary/OpenSchoolLibrary.Data/BookRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OpenSchoolLibrary.Common.Models;
+
+namespace OpenSchoolLibrary.Data
+{
+    public class BookRepository : IBookRepository
+    {
+
+
+        public IEnumerable<Book> GetBooks()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ OpenSchoolLibrary/OpenSchoolLibrary.Data/IBookRepository.cs
@@ -0,0 +1,13 @@
+﻿using OpenSchoolLibrary.Common.Models;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OpenSchoolLibrary.Data
+{
+    public interface IBookRepository
+    {
+        public IEnumerable<Book> GetBooks();
+    }
+}
