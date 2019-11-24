b37d5830538bb34969b6894259e2ba9fa3ce2d99
Programazing <chris@thatamazingprogrammer.com>
2019-11-24 00:52:46 -0500

    Add Connection String to API project

--- OpenSchoolLibrary/OpenSchoolLibrary/Startup.cs
+++ OpenSchoolLibrary/OpenSchoolLibrary/Startup.cs
@@ -5,10 +5,12 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using OpenSchoolLibrary.Data;
 
 namespace OpenSchoolLibrary
 {
@@ -25,6 +27,9 @@ namespace OpenSchoolLibrary
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddDbContext<LibraryContext>(
+                options => options.UseSqlServer(
+                    Configuration.GetConnectionString("LibraryConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
