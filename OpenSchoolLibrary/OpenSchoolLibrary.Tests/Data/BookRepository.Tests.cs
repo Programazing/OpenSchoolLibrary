14fd99c00d0a635c92eb0f966f60c73d0135c391
Programazing <chris@thatamazingprogrammer.com>
2019-11-24 02:14:57 -0500

    Move Fake Data to its own class in Tests

--- OpenSchoolLibrary/OpenSchoolLibrary.Tests/Data/BookRepository.Tests.cs
+++ OpenSchoolLibrary/OpenSchoolLibrary.Tests/Data/BookRepository.Tests.cs
@@ -4,8 +4,8 @@ using Moq;
 using System;
 using System.Collections.Generic;
 using System.Text;
-using OpenSchoolLibrary.Data;
-using OpenSchoolLibrary.Common.Models;
+using OpenSchoolLibrary.Entities.Interfaces;
+using OpenSchoolLibrary.Entities.Models;
 using System.Linq;
 
 namespace OpenSchoolLibrary.Tests.Data
@@ -13,34 +13,25 @@ namespace OpenSchoolLibrary.Tests.Data
     [TestFixture]
     public class BookRepository
     {
-        private IEnumerable<Book> BookList { get; set; }
+        private IEnumerable<Book> Books { get; set; }
 
         public BookRepository()
         {
-            BookList = PopulateBookList();
+            Books = FakeData.GetBookList();
         }
 
         [Test]
         public void GetBooks_ReturnsBooks()
         {
             var mock = new Mock<IBookRepository>();
-            mock.Setup(b => b.GetBooks()).Returns(() => BookList);
+            mock.Setup(b => b.GetBooks()).Returns(() => Books);
 
             var sut = mock.Object.GetBooks();
 
             sut.Count().Should().Be(3);
             sut.FirstOrDefault().Author.Should().Be("James Dewey");
-
         }
 
-        private IEnumerable<Book> PopulateBookList()
-        {
-            return new List<Book>()
-            {
-                { new Book{ Author="James Dewey", BookId=1, Dewey=new Dewey{ }, DeweyID=1, Genre=new Genre{ }, GenreID=1, ISBN=1, SubTitle="", Title="" } },
-                { new Book{ Author="", BookId=2, Dewey=new Dewey{ }, DeweyID=2, Genre=new Genre{ }, GenreID=2, ISBN=2, SubTitle="", Title="" } },
-                { new Book{ Author="", BookId=3, Dewey=new Dewey{ }, DeweyID=3, Genre=new Genre{ }, GenreID=3, ISBN=3, SubTitle="", Title="" } }
-            };
-        }
+        
     }
 }
---
+++ OpenSchoolLibrary/OpenSchoolLibrary.Tests/Data/FakeData.cs
@@ -0,0 +1,35 @@
+﻿using OpenSchoolLibrary.Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OpenSchoolLibrary.Tests.Data
+{
+    internal static class FakeData
+    {
+        public static IEnumerable<Book> GetBookList()
+        {
+            return new List<Book>()
+            {
+                { new Book{ Author="James Dewey", BookId=1, Dewey=new Dewey{ }, DeweyID=1, Genre=new Genre{ }, GenreID=1, ISBN=1, SubTitle="", Title="" } },
+                { new Book{ Author="", BookId=2, Dewey=new Dewey{ }, DeweyID=2, Genre=new Genre{ }, GenreID=2, ISBN=2, SubTitle="", Title="" } },
+                { new Book{ Author="", BookId=3, Dewey=new Dewey{ }, DeweyID=3, Genre=new Genre{ }, GenreID=3, ISBN=3, SubTitle="", Title="" } }
+            };
+        }
+
+        public static IEnumerable<Student> GetStudents()
+        {
+            return new List<Student>()
+            {
+                new Student {Email = "", Fines = 0, FirstName = "John", Grade = 3, IssuedID = 0003,
+                LastName = "Smith", StudentID = 0258, Teacher = new Teacher { }, TeacherID = 458 },
+
+                new Student {Email = "", Fines = 1.00m, FirstName = "Jane", Grade = 4, IssuedID = 0803,
+                LastName = "Smith", StudentID = 0758, Teacher = new Teacher { }, TeacherID = 895 },
+
+                new Student {Email = "", Fines = 0, FirstName = "Mark", Grade = 3, IssuedID = 0753,
+                LastName = "Straw", StudentID = 5486, Teacher = new Teacher { }, TeacherID = 342 }
+            };
+        }
+    }
+}
